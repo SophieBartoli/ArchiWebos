43c4e6a67ed649bfd973f2788fea435f67e16164
Sophie Bartoli <sophie.bartoli2@gmail.com>
2023-10-10 07:22:33 +0200

    Fermeture et ouverture modale fonctionnelle et travail sur ajout travaux

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -11,44 +11,44 @@ fetch ('http://localhost:5678/api/works')
 
             console.log(elements);
 
-            // loop to see the elements in the console
+            // loop pour voir les elements dans la console
             for (let i = 0; i < elements.length; i++) {
                 console.log(elements[i]);
             }
 
-            // loop to create the images in js
+            // loop pour creer les images dans le js
             for (let i = 0; i < elements.length; i++) {
 
-                //creating the figure and the image
+                //creation de la figure et de l'image
                 let figureElement = document.createElement('figure');
                 let imageElement = document.createElement('img');
 
-                imagesArray.push(figureElement); //add each img element into array
+                imagesArray.push(figureElement); //ajout de chaque image dans son array
 
-                //getting the div 
+                //obtenir le div
                 const container = document.getElementById('imgGallery');
 
-                // putting figure in div and the images in figure
+                // mettre la figure dans le div et les images dans la figure
                 container.appendChild(figureElement);
                 figureElement.appendChild(imageElement);
 
 
-                // getting url of images and putting it on image
+                // obtenir l'url des images et les mettre sur chaque image creee
                 let element = elements[i];
                 let values = Object.values(element);
                 let imgUrl = values[2];
                 imageElement.src = imgUrl;
 
-                // getting title of images and putting it in figure
+                // obtenir le titre de chaque image et les mettre sur image
                 let imgText = values[1]; 
                 let imageCaption = document.createElement('figcaption');
                 figureElement.appendChild(imageCaption);
                 imageCaption.innerHTML = values[1];
 
-                // giving each image its category number 
+                // donner le numero de categorie de chaque image
                 let categoryId = values[3];
 
-                catIdArray.push(categoryId); //add category numbers of each img into array
+                catIdArray.push(categoryId); //ajouter les numeros de categories dans une array
 
 
             } 
@@ -64,52 +64,52 @@ fetch ('http://localhost:5678/api/works')
 
                         console.log(categories);
 
-                        // loop to see the categories in the console
+                        // loop pour voir les categories dans la console
                         for (let i = 0; i < categories.length; i++) {
                             console.log(categories[i]);
                         }
 
-                        //getting the categories div 
+                        //obtenir le div des categories depuis HTML
                         const catContainer = document.getElementById('ulCategories');
 
-                        //creating the tous category
+                        //creation de la categorie tous
                         let tousCategory = document.createElement('li');
                         catContainer.appendChild(tousCategory);
                         tousCategory.innerHTML = 'Tous';
                         
-                        catArray.push(tousCategory); //add tous li as index 0 of categories array
+                        catArray.push(tousCategory); //mettre la categorie tous en index 0 dans l'array
 
 
-                        // loop to create the 3 categories in js
+                        // loop pour creer les trois autres categories dans js
                         for (let i = 0; i < categories.length; i++) {
 
-                            //creating the categories
+                            //creation des categories
                             let liCategories = document.createElement('li');
                             
-                            // putting the categories in the categories div
+                            //mettre les categories dans le div des categories
                             catContainer.appendChild(liCategories);
 
-                            // getting categories info and putting them in ul
+                            // obtenir les informations des categories et les mettre dans l'ul
                             let category = categories[i];
                             let catValues = Object.values(category);
                             let catText = catValues[1]; 
                             liCategories.innerHTML = catText;
 
-                            catArray.push(liCategories); //add other li as index 1,2,3 of categories array
+                            catArray.push(liCategories); //mettre les autres categories sur les index 1,2,3
                         }  
 
-                        //naming each index of the categories array
+                        //nommer chaque index avec le nom de categories
                         let buttonTous = catArray[0];
                         let buttonObjets = catArray[1];
                         let buttonApp = catArray[2];
                         let buttonHot = catArray[3];
 
-                        //shows all images when Tous clicked
+                        //montrer toutes les images quand on appuie sur Tous
                          buttonTous.onclick = function () {
                             for (let i = 0; imagesArray.length; i++) {
                                 imagesArray[i].style.display = 'block';
 
-                                //CSS of boutons when clicked
+                                //CSS des boutons lors du clic
                                 buttonTous.style.color = 'white';
                                 buttonTous.style.backgroundColor = '#1D6154'; 
 
@@ -124,13 +124,13 @@ fetch ('http://localhost:5678/api/works')
 
                         } 
 
-                        //shows object images when Objets clicked
+                        //montrer les images d'objets quand on appuie sur Objets
                         buttonObjets.onclick = function () {
                             for (let i = 0; imagesArray.length; i++) {
                                 if (catIdArray[i] === 1) {
                                     imagesArray[i].style.display = 'block';
 
-                                    //CSS of boutons when clicked
+                                    //CSS des boutons lors du clic
                                     buttonObjets.style.color = 'white';
                                     buttonObjets.style.backgroundColor = '#1D6154';
 
@@ -148,13 +148,13 @@ fetch ('http://localhost:5678/api/works')
                             
                         } 
 
-                        //shows appartment images when Appartements clicked
+                        //montrer les images d'appartements quand on appuie sur Appartements
                         buttonApp.onclick = function () {
                             for (let i = 0; imagesArray.length; i++) {
                                 if (catIdArray[i] === 2) {
                                     imagesArray[i].style.display = 'block';
 
-                                    //CSS of boutons when clicked
+                                    //CSS des boutons lors du clic
                                     buttonApp.style.color = 'white';
                                     buttonApp.style.backgroundColor = '#1D6154';
 
@@ -172,13 +172,13 @@ fetch ('http://localhost:5678/api/works')
                             
                         } 
 
-                        //shows hotel images when Hotel clicked
+                        //montrer les images d'hotel quand on appuie sur Hotel
                         buttonHot.onclick = function () {
                             for (let i = 0; imagesArray.length; i++) {
                                 if (catIdArray[i] === 3) {
                                     imagesArray[i].style.display = 'block';
 
-                                    //CSS of boutons when clicked
+                                    //CSS des boutons lors du clic
                                     buttonHot.style.color = 'white';
                                     buttonHot.style.backgroundColor = '#1D6154';
 
@@ -196,16 +196,16 @@ fetch ('http://localhost:5678/api/works')
                             
                         } 
 
-                        // css of the categories
+                        // CSS des categories
                         catContainer.style.display = 'flex';
                         catContainer.style.justifyContent = 'center';
 
                         buttonTous.style.display = 'block';
                         buttonTous.style.margin = '10px 10px 40px';
                         buttonTous.style.fontFamily = 'Syne';
-                        buttonTous.style.color = "#1D6154"; 
-                        buttonTous.style.border = "1px solid #1D6154";
-                        buttonTous.style.borderRadius = "20px";
+                        buttonTous.style.color = '#1D6154'; 
+                        buttonTous.style.border = '1px solid #1D6154';
+                        buttonTous.style.borderRadius = '20px';
                         buttonTous.style.padding = '10px 20px';
                         buttonTous.style.cursor = 'pointer';
 
@@ -213,9 +213,9 @@ fetch ('http://localhost:5678/api/works')
                         buttonObjets.style.display = 'block';
                         buttonObjets.style.margin = '10px 10px 40px';
                         buttonObjets.style.fontFamily = 'Syne';
-                        buttonObjets.style.color = "#1D6154"; 
-                        buttonObjets.style.border = "1px solid #1D6154";
-                        buttonObjets.style.borderRadius = "20px";
+                        buttonObjets.style.color = '#1D6154'; 
+                        buttonObjets.style.border = '1px solid #1D6154';
+                        buttonObjets.style.borderRadius = '20px';
                         buttonObjets.style.padding = '10px 20px';
                         buttonObjets.style.cursor = 'pointer';
 
@@ -223,9 +223,9 @@ fetch ('http://localhost:5678/api/works')
                         buttonApp.style.display = 'block';
                         buttonApp.style.margin = '10px 10px 40px';
                         buttonApp.style.fontFamily = 'Syne';
-                        buttonApp.style.color = "#1D6154"; 
-                        buttonApp.style.border = "1px solid #1D6154";
-                        buttonApp.style.borderRadius = "20px";
+                        buttonApp.style.color = '#1D6154'; 
+                        buttonApp.style.border = '1px solid #1D6154';
+                        buttonApp.style.borderRadius = '20px';
                         buttonApp.style.padding = '10px 20px';
                         buttonApp.style.cursor = 'pointer';
 
@@ -233,21 +233,20 @@ fetch ('http://localhost:5678/api/works')
                         buttonHot.style.display = 'block';
                         buttonHot.style.margin = '10px 10px 40px';
                         buttonHot.style.fontFamily = 'Syne';
-                        buttonHot.style.color = "#1D6154"; 
-                        buttonHot.style.border = "1px solid #1D6154";
-                        buttonHot.style.borderRadius = "20px";
+                        buttonHot.style.color = '#1D6154'; 
+                        buttonHot.style.border = '1px solid #1D6154';
+                        buttonHot.style.borderRadius = '20px';
                         buttonHot.style.padding = '10px 20px';
                         buttonHot.style.cursor = 'pointer';
                         
-                        // removing login button and putting logout button when logged in
-
-                        let loginButton = document.getElementById("loginButton");
-                        let logoutButton = document.getElementById("logoutButton");
-                        let ulCategories = document.getElementById("ulCategories");
-                        let projetsDiv = document.getElementById("projetsDiv");
+                        // enlever le bouton login et mettre le bouton logout quand on est connectes
+                        let loginButton = document.getElementById('loginButton');
+                        let logoutButton = document.getElementById('logoutButton');
+                        let ulCategories = document.getElementById('ulCategories');
+                        let projetsDiv = document.getElementById('projetsDiv');
                         
 
-                        if (sessionStorage.token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5NjUxMjI3NywiZXhwIjoxNjk2NTk4Njc3fQ.t30pQ39-PANtdVzy9ki9EFX_GgAA4EFFJyuTamk_oWI" && sessionStorage.userId == 1) {
+                        if (sessionStorage.token == 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5NjUxMjI3NywiZXhwIjoxNjk2NTk4Njc3fQ.t30pQ39-PANtdVzy9ki9EFX_GgAA4EFFJyuTamk_oWI' && sessionStorage.userId == 1) {
                             loginButton.style.display = 'none';
                             logoutButton.style.display = 'block';
                             ulCategories.style.display = 'none';
@@ -269,28 +268,33 @@ fetch ('http://localhost:5678/api/works')
 
     }) 
 
-//doing the action of logging out
+//action du bouton logout
 
 let logoutButton = document.getElementById('logoutButton');
 let modifierButton = document.getElementById('modifierButton');
 let boiteModal = document.getElementById('boiteModal');
 let modalClose = document.getElementById('modalClose');
+let closeTest = document.getElementById('closeTest');
 
-logoutButton.addEventListener("click", function (event) {
+logoutButton.addEventListener('click', function (event) {
     sessionStorage.clear();
     window.location.reload();
     
 })
 
-//doing the action of modifying the gallery
-modifierButton.addEventListener("click", function (event) {
-    boiteModal.style.display = "flex";
+//action du bouton modifier 
+modifierButton.addEventListener('click', function (event) {
+    boiteModal.style.display = 'flex';
 })
 
+//fermeture de la fenetre modale lors du clic sur la croix ou hors de la fenetre
 modalClose.addEventListener('click', function (event) {
-    boiteModal.style.display = "none";
+    boiteModal.style.display = 'none';
 })
 
+closeTest.addEventListener('click', function (event) {
+    boiteModal.style.display = 'none';
+})
 
 //fetch de la fenêtre modale
 let modalWorks = document.getElementById('modalWorks');
@@ -315,3 +319,5 @@ fetch ('http://localhost:5678/api/works')
 
     })
 })
+
+//fetch pour supprimer des projets
--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,5 +1,5 @@
 
-
+// fonction pour se connecter 
 function ajoutListenerLogin() {
 
     const form = document.getElementById("form");
@@ -16,6 +16,7 @@ function ajoutListenerLogin() {
     
         const chargeUtile = JSON.stringify(login);
     
+        //fetch pour obtenir les identifiants
         fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -31,6 +32,7 @@ function ajoutListenerLogin() {
     
         .then(data => {
             
+            //enregistrement des identifiants en local
             data.json().then(token => {
                 console.log(token);
                 localStorage.setItem("token", token.token);
@@ -44,6 +46,7 @@ function ajoutListenerLogin() {
             console.error("Il y a eu un problème avec l'opération fetch:", error);
         });
 
+        //redirection sur page d'accueil apres connection et enregistrement des identifiants en session
         if (login.email == "sophie.bluel@test.tld" && login.password == "S0phie") {
             window.location.assign("file:///C:/Users/Sophie/Desktop/PROJETARCHIWEBOS/FrontEnd/index.html");
             sessionStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5NjUxMjI3NywiZXhwIjoxNjk2NTk4Njc3fQ.t30pQ39-PANtdVzy9ki9EFX_GgAA4EFFJyuTamk_oWI");
